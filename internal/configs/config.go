d545fca54539ce7242a7b7d39a761e05b94f5733
HACK3R911 <afanasiev-2004@list.ru>
2025-04-28 12:35:27 +0300

    feat: реализация загрузки конфига из config.yaml

--- internal/configs/config.go
+++ internal/configs/config.go
@@ -1,1 +1,25 @@
 package configs
+
+import (
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	Port     string   `yaml:"port"`
+	Backends []string `yaml:"backends"`
+}
+
+func Load(path string) (*Config, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	var cfg Config
+	if err := yaml.Unmarshal(data, &cfg); err != nil {
+		return nil, err
+	}
+	return &cfg, nil
+}
