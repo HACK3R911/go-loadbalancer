6caa19ad2c4cbe1984c02fe4d1c827cf90c86294
HACK3R911 <afanasiev-2004@list.ru>
2026-01-15 19:43:59 +0300

    add limiterÐ, healthchecks

--- internal/server/server.go
+++ internal/server/server.go
@@ -2,23 +2,30 @@ package server
 
 import (
 	"cloudru/internal/logger"
+	"cloudru/internal/proxy"
 	"context"
+	"golang.org/x/time/rate"
 	"net/http"
 )
 
 type Server struct {
 	httpServer *http.Server
 	logger     *logger.Logger
+	proxy      *proxy.Proxy
+	limiter    *rate.Limiter
 }
 
-func New(logger *logger.Logger) *Server {
-	return &Server{logger: logger}
+func New(logger *logger.Logger, p *proxy.Proxy, rl rate.Limit, burst int) *Server {
+	s := &Server{logger: logger, proxy: p}
+	mux := http.NewServeMux()
+	mux.Handle("/", rateLimitMiddleware(p, rl, burst))
+	mux.Handle("/health", http.HandlerFunc(s.health))
+	return s
 }
 
-func (s *Server) Run(port string /*, _ http.Handler*/) error {
+func (s *Server) Run(port string) error {
 	s.httpServer = &http.Server{
 		Addr: ":" + port,
-		//Handler: loggingMiddleware(handler),
 	}
 	s.logger.Infof("Запуск сервера на порту %s", port)
 	return s.httpServer.ListenAndServe()
@@ -29,9 +36,17 @@ func (s *Server) Shutdown(ctx context.Context) error {
 	return s.httpServer.Shutdown(ctx)
 }
 
-//func loggingMiddleware(next http.Handler) http.Handler {
-//	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-//		log.Printf("%s %s", r.Method, r.URL.Path)
-//		next.ServeHTTP(w, r)
-//	})
-//}
+func rateLimitMiddleware(next http.Handler, r rate.Limit, burst int) http.HandlerFunc {
+	limiter := rate.NewLimiter(r, burst)
+	return func(w http.ResponseWriter, req *http.Request) {
+		if !limiter.Allow() {
+			http.Error(w, "Rate limit exceeded", http.StatusTooManyRequests)
+			return
+		}
+		next.ServeHTTP(w, req)
+	}
+}
+func (s *Server) health(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
